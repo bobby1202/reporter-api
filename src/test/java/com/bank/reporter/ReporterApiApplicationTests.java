aa80db26ae0ddeae354af2eeec9113c22b9c2f1c
vpudi <vara.pudi@booking.com>
2023-11-27 19:55:45 +0100

    Initial Code Commit

---
+++ src/test/java/com/bank/reporter/ReporterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.reporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReporterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
